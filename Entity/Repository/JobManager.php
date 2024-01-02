24c85b3cb0883b86ac10cee32457b41ccd07aada
Felix Nagel <info@felixnagel.com>
2024-01-02 15:20:44 +0100

    Replace outdated json_array ORM type

--- Entity/Repository/JobManager.php
+++ Entity/Repository/JobManager.php
@@ -77,7 +77,7 @@ class JobManager
 
         $firstJob = $this->getJobManager()->createQuery("SELECT j FROM JMSJobQueueBundle:Job j WHERE j.command = :command AND j.args = :args ORDER BY j.id ASC")
              ->setParameter('command', $command)
-             ->setParameter('args', $args, 'json_array')
+             ->setParameter('args', $args, Types::JSON)
              ->setMaxResults(1)
              ->getSingleResult();
 
